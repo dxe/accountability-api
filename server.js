e083a7ca44ef512404a32822f432f32343da9f6c
EC2 Server <tech@dxe.io>
2019-12-07 09:09:23 -0800

    only run jobs on one instance

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 
-const alerts = require("./services/alerts");
+// only run alerts service on "instance 0" when in prod
+if (process.env.NODE_APP_INSTANCE == 0 ||  process.env.NODE_ENV === "development") {
+	const alerts = require("./services/alerts");
+}
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -27,4 +30,4 @@ app.use("/users", usersRouter);
 const accomplishmentsRouter = require("./routes/accomplishments");
 app.use("/accomplishments", accomplishmentsRouter);
 
-app.listen(process.env.PORT, () => console.log("Server started"));
+app.listen(process.env.PORT, () => console.log(`Server started: ${process.env.NODE_ENV} instance ${process.env.NODE_APP_INSTANCE}.`));
